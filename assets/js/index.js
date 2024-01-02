6cf7e73a8a669377e5241962e05f3b5da9ea5973
Dimbb <bogomolovdmitrij535@gmail.com>
2024-01-02 23:14:42 +0300

    added content in startvalues

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,11 @@
 // Imports
 
 import { Tabs } from "./modules/Tabs.js";
-import { clickers, busineses, factors, timer, settings, business, counter, statistic } from "./modules/startValues.js";
+import { clickers, busineses, factors, timer, settings, business, counter, statistic, tabs as tabsSettings } from "./modules/startValues.js";
 import { ClickerService, BusinessService, FactorService } from "./modules/Services.js";
 import { Clicker } from "./modules/Clicker.js";
 
-const tabs = new Tabs({
-    tabs: ".clicker__tab",
-    buttons: ".clicker__list-btn",
-    activeClass: "clicker__list-btn_active",
-    noneClass: "tab_none"
-});
+const tabs = new Tabs(tabsSettings);
 
 const clicker = new Clicker({
     timer: timer,
--- assets/js/modules/startValues.js
+++ assets/js/modules/startValues.js
@@ -9,28 +9,112 @@ export const clickers = [
         name: "Деревянный клик",
         infoArray: [
             { name: "Цена:", value: "100р." },
-            { name: "Доходность:", value: "10р./Клик" },
+            { name: "Доходность:", value: "1.5р./Клик" },
         ],
         level: 1,
         price: 100,
+        type: "clicker",
+        countPerClick: 1.5,
+        parentEl: ".tab__content_services-clickers",
+    },
+
+    {
+        img: "fa-solid fa-gem",
+        name: "Каменный клик",
+        infoArray: [
+            { name: "Цена:", value: "1000р." },
+            { name: "Доходность:", value: "2р./Клик" },
+        ],
+        level: 2,
+        price: 1000,
+        type: "clicker",
+        countPerClick: 2,
+        parentEl: ".tab__content_services-clickers",
+    },
+
+    {
+        img: "fa-solid fa-gem",
+        name: "Бронзовый клик",
+        infoArray: [
+            { name: "Цена:", value: "10000р." },
+            { name: "Доходность:", value: "5р./Клик" },
+        ],
+        level: 3,
+        price: 10000,
+        type: "clicker",
+        countPerClick: 5,
+        parentEl: ".tab__content_services-clickers",
+    },
+
+    {
+        img: "fa-solid fa-paperclip",
+        name: "Железный клик",
+        infoArray: [
+            { name: "Цена:", value: "20000р." },
+            { name: "Доходность:", value: "10р./Клик" },
+        ],
+        level: 4,
+        price: 20000,
         type: "clicker",
         countPerClick: 10,
         parentEl: ".tab__content_services-clickers",
     },
 
     {
-        img: "fa-brands fa-pagelines",
-        name: "Деревянный клик",
+        img: "fa-solid fa-coins",
+        name: "Золотой клик",
         infoArray: [
-            { name: "Цена:", value: "1000р." },
-            { name: "Доходность:", value: "10р./Клик" },
+            { name: "Цена:", value: "30000р." },
+            { name: "Доходность:", value: "20р./Клик" },
         ],
-        level: 2,
-        price: 1000,
+        level: 5,
+        price: 30000,
+        type: "clicker",
+        countPerClick: 20,
+        parentEl: ".tab__content_services-clickers",
+    },
+
+    {
+        img: "fa-solid fa-medal",
+        name: "Серебряный клик",
+        infoArray: [
+            { name: "Цена:", value: "40000р." },
+            { name: "Доходность:", value: "50р./Клик" },
+        ],
+        level: 6,
+        price: 40000,
+        type: "clicker",
+        countPerClick: 50,
+        parentEl: ".tab__content_services-clickers",
+    },
+
+    {
+        img: "fa-solid fa-sheet-plastic",
+        name: "Платиновый клик",
+        infoArray: [
+            { name: "Цена:", value: "100000р." },
+            { name: "Доходность:", value: "100р./Клик" },
+        ],
+        level: 7,
+        price: 100000,
         type: "clicker",
         countPerClick: 100,
         parentEl: ".tab__content_services-clickers",
-    }
+    },
+
+    {
+        img: "fa-regular fa-gem",
+        name: "Рубиновый клик",
+        infoArray: [
+            { name: "Цена:", value: "200000р." },
+            { name: "Доходность:", value: "200р./Клик" },
+        ],
+        level: 8,
+        price: 200000,
+        type: "clicker",
+        countPerClick: 200,
+        parentEl: ".tab__content_services-clickers",
+    },
 
 ];
 
@@ -46,25 +130,67 @@ export const busineses = [
             { name: "Доходность:", value: "1р./Сек" },
         ],
         level: 1,
-        price: 1,
+        price: 1000,
         type: "business",
         countPerSecond: 1,
         parentEl: ".tab__content_services-business",
     },
 
     {
-        img: "fa-brands fa-pagelines",
+        img: "fa-solid fa-gem",
         name: "Шахта",
         infoArray: [
             { name: "Цена:", value: "10 000р." },
-            { name: "Доходность:", value: "10р./Сек." },
+            { name: "Доходность:", value: "5р./Сек." },
         ],
         level: 2,
-        price: 10,
+        price: 10_000,
         type: "business",
-        countPerSecond: 2,
+        countPerSecond: 5,
         parentEl: ".tab__content_services-business",
-    }
+    },
+
+    {
+        img: "fa-solid fa-martini-glass",
+        name: "Бар",
+        infoArray: [
+            { name: "Цена:", value: "100 000р." },
+            { name: "Доходность:", value: "50р./Сек." },
+        ],
+        level: 3,
+        price: 100_000,
+        type: "business",
+        countPerSecond: 50,
+        parentEl: ".tab__content_services-business",
+    },
+
+    {
+        img: "fa-solid fa-car",
+        name: "Автомойка",
+        infoArray: [
+            { name: "Цена:", value: "1 000 000р." },
+            { name: "Доходность:", value: "500р./Сек." },
+        ],
+        level: 4,
+        price: 1_000_000,
+        type: "business",
+        countPerSecond: 500,
+        parentEl: ".tab__content_services-business",
+    },
+
+    {
+        img: "fa-solid fa-laptop-code",
+        name: "Фронтендерский стартап ( Рабский )",
+        infoArray: [
+            { name: "Цена:", value: "10 000 000р." },
+            { name: "Доходность:", value: "5000р./Сек." },
+        ],
+        level: 5,
+        price: 10_000_000,
+        type: "business",
+        countPerSecond: 5000,
+        parentEl: ".tab__content_services-business",
+    },
 
 ];
 
@@ -76,11 +202,11 @@ export const factors = [
         img: "x1.1",
         name: "Множитель x1.1",
         infoArray: [
-            { name: "Цена:", value: "1 000р." },
+            { name: "Цена:", value: "1 млн. р." },
             { name: "Множитель:", value: "x1.1" },
         ],
         level: 1,
-        price: 1,
+        price: 1_000_000,
         type: "factor",
         countFactor: 1.1,
         parentEl: ".tab__content_services-factors",
@@ -90,20 +216,61 @@ export const factors = [
         img: "x1.2",
         name: "Множитель x1.2",
         infoArray: [
-            { name: "Цена:", value: "10 000р." },
+            { name: "Цена:", value: "10 млн. р." },
             { name: "Множитель:", value: "x1.2" },
         ],
         level: 2,
-        price: 10,
+        price: 10_000_000,
         type: "factor",
         countFactor: 1.2,
         parentEl: ".tab__content_services-factors",
-    }
+    },
+
+    {
+        img: "x1.3",
+        name: "Множитель x1.3",
+        infoArray: [
+            { name: "Цена:", value: "100 млн. р." },
+            { name: "Множитель:", value: "x1.3" },
+        ],
+        level: 3,
+        price: 100_000_000,
+        type: "factor",
+        countFactor: 1.3,
+        parentEl: ".tab__content_services-factors",
+    },
+
+    {
+        img: "x1.4",
+        name: "Множитель x1.4",
+        infoArray: [
+            { name: "Цена:", value: "1 млрд. р." },
+            { name: "Множитель:", value: "x1.4" },
+        ],
+        level: 4,
+        price: 1_000_000_000,
+        type: "factor",
+        countFactor: 1.4,
+        parentEl: ".tab__content_services-factors",
+    },
+
+    {
+        img: "x1.5",
+        name: "Множитель x1.5",
+        infoArray: [
+            { name: "Цена:", value: "10 млрд. р." },
+            { name: "Множитель:", value: "x1.5" },
+        ],
+        level: 5,
+        price: 10_000_000_000,
+        type: "factor",
+        countFactor: 1.5,
+        parentEl: ".tab__content_services-factors",
+    },
 
 ];
 
 // Clicker Settings
-
 
 const timer = { startTime: 0 };
 const business = { name: "Отсутствует" };
@@ -127,4 +294,11 @@ const statistic = {
     time: 0,
 }
 
-export { timer, settings, business, counter, statistic };
+const tabs = {
+    tabs: ".clicker__tab",
+    buttons: ".clicker__list-btn",
+    activeClass: "clicker__list-btn_active",
+    noneClass: "tab_none",
+}
+
+export { timer, settings, business, counter, statistic, tabs };
